3773c44f5acbf93b523d974e8b5fa497c7c477f2
Chase Carnaroli <chasecarnaroli@gmail.com>
2019-12-25 05:34:13 -0800

    Add calendar tracking automation
    Every 5 mins, duty bot will refresh the calendar and post any difference

--- duty_bot.py
+++ duty_bot.py
@@ -16,10 +16,13 @@ ical_url = SECRETS.get("ical_url")
 oauth_token = SECRETS.get("oauth_token")
 slack_client = slack.WebClient(token=oauth_token)
 
-# Post Slack Message
+# Post Slack Duty Message
 #   Posts a message to the channel set in global variables
 #   Sending it as an attachment with the color #1f4387,
 #       posts the message with the mesa blue line next to it
+# TODO: rename to `post_attachment_slack_message`, 
+#       kick parse_for_users out to the func that called this one,
+#       combine with `post_calendar_slack_message`
 def post_duty_slack_message(message: str):
     parsed_message = parse_for_users(message)
     slack_client.chat_postMessage(
@@ -32,10 +35,30 @@ def post_duty_slack_message(message: str):
         ]
     )
 
+
+# Post Calendar Slack Message
+# TODO: see above
+def post_calendar_slack_message(pretext: str, message: str):
+    print(pretext)
+    print(message)
+    
+    slack_client.chat_postMessage(
+        channel = channel,
+        attachments = [
+            {
+                "color": "#1f4387",
+                "pretext": pretext,
+                "text": message
+            }
+        ]
+    )
+
+
+
 # Parse For User
 #   Given a str, parse it and swap out any user names with user IDS
 #   This will @ the person when the message is posted
-def parse_for_users(text: str):
+def parse_for_users(text: str) -> str:
     parsed_text = []
     
     for word in text.split(":"):
@@ -48,6 +71,19 @@ def parse_for_users(text: str):
     return ": ".join(parsed_text)  
 
 
+# Date to String
+#   Given a str in ical date format (YYYYMMDD),
+#   Return it in a readable format (MM/DD/YYYY)
+#   TODO: Use dict to swape out month number with month name,
+#           format should be (Month DD, YYYY)
+def date_to_string(date: str) -> str:
+    year = date[0:4]
+    month = date[4:6]
+    day = date[6:]
+    return f"{month}/{day}/{year}"
+
+
+# Post Daily Duty Schedule
 def post_daily_duty_schedule():
     # Generate Calendar Dictionary
     #   {str : [dict]}
@@ -70,11 +106,64 @@ def post_daily_duty_schedule():
         post_duty_slack_message(member)
 
 
+# Check for Calendar Updates
+#   Call refresh on calendar, 
+#   Check to see if there are differences between prev and current version,
+#   Post the differences to Slack,
+#   TODO: Log the differences (to a text file?)  
+#
+#   Note: Since python paramaters are `pass-by-object-reference`,
+#   calling refresh on calendar updates the variable in __main__.
+def check_for_calendar_updates(calendar):
+    # Get a list of differences between the current and previous versions
+    # List format: [{ change_type: str, event: dict, previous: ?dict }]
+    differences = calendar.refresh(compare=True)
+    
+    if not len(differences) == 0:
+        for difference in differences:
+            # Pull some variables out of `difference
+            change_type = difference["change_type"]
+            event = difference["event"]
+            event_summary = event["SUMMARY"]
+            event_date = date_to_string(event["DTSTART;VALUE=DATE"])   
+
+            # Post messages to slack, depending on the change_type
+            if change_type == "ADDITION":
+                post_calendar_slack_message(
+                    "Calendar event was added :rotating_light:",
+                    "*{event_title}*\n{date}".format(
+                        event_title = parse_for_users(event_summary),
+                        date = event_date
+                    )
+                )
+            elif change_type == "UPDATE":
+                post_calendar_slack_message(
+                    "Calendar event was updated",
+                    "*~{old_event}~ → {new_event}*\n{date}".format(
+                        old_event = parse_for_users(difference["previous_event"]["SUMMARY"]),
+                        new_event = parse_for_users(event_summary),
+                        date = event_date
+                    )
+                )
+            elif change_type == "REMOVAL":
+                post_calendar_slack_message(
+                    "Calendar event was deleted :rotating_light:",
+                    "*~{event_title}~*\n{date}".format(
+                        event_title = parse_for_users(event_summary),
+                        date = event_date
+                    )
+                )
+
+
 # Main
 if __name__ == "__main__":
+    # Create calendar ref
+    calendar = iCal(ical_url)
+
     # Schedule jobs
+    schedule.every(5).minutes.do(check_for_calendar_updates, calendar)
     schedule.every().day.at("16:00").do(post_daily_duty_schedule)    
 
     while True:
         schedule.run_pending()
-        time.sleep(30)    
+        time.sleep(20)    
